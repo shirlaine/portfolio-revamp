68b05209cf66a2a17c111f8fedc2258ce3e9ffdb
shirlaine <shirlaine.phang@gmail.com>
2017-10-10 21:41:19 +0800

    Add pages controllers, routes and views

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def landing_page; end
+
+  def about_me; end
+
+  def contact_me; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/landing-page', to: 'pages#landing_page'
+
+  get '/about-me', to: 'pages#about_me'
+
+  get '/contact-me', to: 'pages#contact_me'
 end
